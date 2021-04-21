c1120ce1d18088457b193717e8bac721a3863e4a
weianke <245783065@qq.com>
2021-04-21 18:28:38 +0800

    新增

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import Enzyme, { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+Enzyme.configure({ adapter: new Adapter() });
+
+// 单元测试应用shallow  集成测试用 mount
+it('renders learn react link', () => {
+  const wrapper = shallow(<App />);
+  // expect(wrapper.find('[data-test="container"]').length).toBe(1);
+  // expect(wrapper.find('[data-test="container"]').prop('title')).toBe('dell lee');
+  expect(wrapper.find('[data-test="container"]')).toHaveProp('title', 'dell lee');
 });
