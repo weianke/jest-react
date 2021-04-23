81297759efff549b099b56fe7929ccf97d001a9f
anke <245783065@qq.com>
2021-04-24 01:12:24 +0800

    修改

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Test from './test'
 
 function App() {
   return (
     // data-test 给单元测试使用的属性
     <div className="app-container" title="dell lee" data-test="container">
       Hello world
+      <Test></Test>  
     </div>
   );
 }
---
+++ src/test.js
@@ -0,0 +1,48 @@
+import React, { Component } from 'react';
+
+class Test extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      count: 0,
+      flag: true,
+    };
+  }
+
+  getData = () => {
+    this.setState({
+      count: this.state.count + 1,
+    });
+
+    fetch('/').then(() => {
+      // 接口返回为 true, 清除定时器加载页面。否则继续定时器，并发请求
+      if (this.state.flag) {
+        clearInterval(this.timer);
+        // this.setState({
+        //   count: 0,
+        // });
+      }
+    });
+  };
+
+  componentDidMount() {
+    this.timer = setInterval(() => {
+      if (this.state.count === 7) {
+        clearInterval(this.timer);
+        // 提示弹框
+        return;
+      }
+      this.getData();
+    }, 3000);
+  }
+
+  componentWillUnmount() {
+    this.timer && clearInterval(this.timer);
+  }
+
+  render() {
+    return <div>{this.state.count}</div>;
+  }
+}
+
+export default Test;
